e1269534cd37c5030c347ba7b883f464f3a04575
Robert Herber <rwherber@gmail.com>
2022-01-24 07:52:15 -0800

    docs: update readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # algo-visualizer
 
-See demo: https://rob893.github.io/algo-visualizer
+See application here: https://rob893.github.io/algo-visualizer
+
+This application visualizes various pathfinding algorithms.
+This stands out from the many other pathfinding visualizers out there in that all of the pathfinding logic is written in Rust and compiled to web assembly to be ran at runtime.
--- app/src/components/AboutDialog.tsx
+++ app/src/components/AboutDialog.tsx
@@ -17,7 +17,11 @@ export default function AboutDialog({ open, onClose }: AboutDialogProps): JSX.El
     >
       <DialogTitle id="about-dialog-title">About</DialogTitle>
       <DialogContent>
-        <p>Some stuff about this site.</p>
+        <p>This application visualizes various pathfinding algorithms.</p>
+        <p>
+          This stands out from the many other pathfinding visualizers out there in that all of the pathfinding logic is
+          written in Rust and compiled to web assembly to be ran at runtime.
+        </p>
       </DialogContent>
       <DialogActions>
         <Button onClick={() => onClose()}>Close</Button>
