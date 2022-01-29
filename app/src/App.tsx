0414549e253f5e1f51b211bfe8340d477c3d1b58
Robert Herber <rwherber@gmail.com>
2022-01-28 18:29:38 -0800

    docs: add comment [ci skip]

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,6 +7,15 @@ import { PathFindingAlgorithm } from './wasm/algo_visualizer';
 import Legend from './components/Legend';
 import { localStorageService } from './services/LocalStorageService';
 
+/**
+ * !!! TO ANYONE READING !!!
+ * There are a lot of 'non-react' ways of doing things in this project (rxjs, document.getElementById, etc) due to performance issues.
+ * Updating the class name using state or keeping the grid immutable while lifting state up by copying it every mutation caused huge amounts of lag
+ * (copying a 1-2k element array then re-rendering the entire grid multiple times per second while drawing walls or animating paths is silly).
+ * To fix this, the methods chosen were used which eliminated all performance issues.
+ *
+ * I am sure there is a more 'react' way to do it but  ¯\_(ツ)_/¯
+ */
 function App(): JSX.Element {
   const gridWidth = 60;
   const gridHeight = 25;
