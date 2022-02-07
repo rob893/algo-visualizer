289ac64d66ee0dbea14712241c595a23cb0e9ad5
Robert Herber <rwherber@gmail.com>
2022-02-07 07:46:44 -0800

    chore: add text

--- app/src/components/AboutDialog.tsx
+++ app/src/components/AboutDialog.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Tab, Tabs, Typography } from '@mui/material';
-import { SyntheticEvent, useState } from 'react';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Link, Tab, Tabs } from '@mui/material';
+import { ReactNode, SyntheticEvent, useState } from 'react';
 
 interface TabPanelProps {
-  children?: React.ReactNode;
+  children?: ReactNode;
   index: number;
   value: number;
 }
@@ -12,11 +12,7 @@ function TabPanel(props: TabPanelProps): JSX.Element {
 
   return (
     <div role="tabpanel" hidden={value !== index} id={`tabpanel-${index}`} aria-labelledby={`tab-${index}`} {...other}>
-      {value === index && (
-        <Box sx={{ paddingTop: 2 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <Box sx={{ paddingTop: 2 }}>{children}</Box>}
     </div>
   );
 }
@@ -64,81 +60,47 @@ export default function AboutDialog({ open, onClose }: AboutDialogProps): JSX.El
           </Tabs>
         </Box>
         <TabPanel value={value} index={0}>
-          This application visualizes various pathfinding algorithms.
-          <br />
-          <br />
-          This stands out from the many other pathfinding visualizers out there in that all of the pathfinding logic is
-          written in Rust and compiled to web assembly to be ran at runtime.
+          <p>This application visualizes various pathfinding algorithms.</p>
+
+          <p>
+            This stands out from the many other pathfinding visualizers out there in that all of the pathfinding logic
+            is written in Rust and compiled to web assembly to be ran at runtime.
+          </p>
         </TabPanel>
         <TabPanel value={value} index={1}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec molestie odio. Maecenas volutpat lacus at
-          sagittis ultrices. Vivamus viverra mi vel neque hendrerit congue. Sed lobortis nec tortor sit amet eleifend.
-          Vestibulum ultrices sollicitudin aliquet. Praesent dolor justo, sollicitudin a faucibus in, aliquam vel augue.
-          Nam facilisis scelerisque justo sed lacinia. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-          posuere cubilia curae; Sed pulvinar id nisl in malesuada. Praesent posuere mauris sed justo elementum, gravida
-          efficitur nisi tincidunt. Morbi posuere sit amet purus quis egestas. Donec ac lacus quam. Praesent eleifend
-          risus dui, ut tempor quam vulputate in. Maecenas eu sapien cursus ante pharetra blandit. Sed ultricies
-          facilisis dictum. Vestibulum luctus dui id elit gravida, a pellentesque purus pharetra. Aliquam lobortis
-          volutpat neque, ac luctus lectus ullamcorper et. Suspendisse tempor turpis id tortor molestie, non rhoncus
-          lacus egestas. Ut hendrerit, tellus non venenatis fringilla, arcu ipsum tincidunt arcu, ut condimentum sapien
-          est sit amet dui. Maecenas turpis libero, sollicitudin eu eros et, eleifend molestie lorem. Maecenas iaculis
-          pulvinar purus. Duis gravida tempor felis vel sollicitudin. Nam ut posuere dui. Pellentesque vulputate, arcu
-          eu mattis rhoncus, lorem risus vulputate nunc, sed condimentum tortor arcu eu neque. Aliquam dignissim sapien
-          non ligula malesuada, in vestibulum metus sagittis. Mauris imperdiet odio magna, id consectetur libero auctor
-          eleifend. Suspendisse potenti. Nunc venenatis et ante sed gravida. Nunc sit amet augue sit amet dui mollis
-          ornare vel non justo. Fusce ac aliquam eros. Donec maximus cursus mauris, sit amet viverra quam eleifend ac.
-          Phasellus nunc odio, blandit euismod sollicitudin ut, fringilla nec quam. Etiam ultricies in lectus nec
-          fringilla. Vestibulum porttitor pellentesque arcu, a euismod nulla mattis ac. Nunc sed sem egestas, blandit
-          ipsum nec, tempus nunc. Donec at augue ligula. Fusce nec vestibulum nunc. Orci varius natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Integer congue risus velit. Aenean elementum, nisl a
-          luctus vehicula, elit augue dignissim justo, at volutpat eros nisl nec mi. Vestibulum aliquam risus nec
-          rhoncus suscipit. Curabitur non eleifend dui. Integer vitae justo a eros blandit tempus. Vestibulum bibendum,
-          dui et pulvinar rutrum, purus arcu ultrices ipsum, id auctor ex ligula at justo. Nulla mollis, sapien quis
-          ultricies sagittis, leo risus posuere elit, ac tincidunt massa metus nec urna. Phasellus a ligula eleifend dui
-          vestibulum venenatis. Duis pulvinar sem facilisis pulvinar suscipit. Proin enim tellus, porta id lectus
-          facilisis, porta pulvinar tellus. Nam non gravida felis. Nullam vulputate vitae turpis ac tempus. Sed urna
-          nulla, sagittis in commodo vitae, venenatis quis nisl. Pellentesque consectetur eros ac blandit porta. Sed sit
-          amet nunc eget enim pharetra accumsan. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Curabitur in convallis quam. Vivamus et odio at sapien ornare auctor id ac diam. Sed
-          sollicitudin, diam ut volutpat tristique, nunc enim laoreet velit, eu ullamcorper augue nulla eu purus. Cras
-          sed mauris turpis. Duis vehicula ultrices aliquet. Proin at rhoncus magna. Duis non nisi ut sem ullamcorper
-          interdum sit amet a nulla. Aliquam erat volutpat. Nullam ac feugiat ligula. Nulla facilisi. Pellentesque
-          habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+          <p>
+            <Link href="https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm" target="_blank" rel="noopener">
+              Dijkstra's Algorithm
+            </Link>{' '}
+            is a <strong>weighted</strong> algorithm that guarantees the shortest path.
+          </p>
+          <p>
+            <Link href="https://en.wikipedia.org/wiki/A*_search_algorithm" target="_blank" rel="noopener">
+              A* Search
+            </Link>{' '}
+            is a <strong>weighted</strong> algorithm that guarantees the shortest path.
+          </p>
+          <p>
+            <Link href="https://en.wikipedia.org/wiki/Best-first_search#Greedy_BFS" target="_blank" rel="noopener">
+              Greedy Best First Search
+            </Link>{' '}
+            is a <strong>weighted</strong> algorithm that does not guarantee the shortest path.
+          </p>
+          <p>
+            <Link href="https://en.wikipedia.org/wiki/Breadth-first_search" target="_blank" rel="noopener">
+              Breadth First Search
+            </Link>{' '}
+            is an <strong>unweighted</strong> algorithm that guarantees the shortest path.
+          </p>
+          <p>
+            <Link href="https://en.wikipedia.org/wiki/Depth-first_search" target="_blank" rel="noopener">
+              Depth First Search
+            </Link>{' '}
+            is an <strong>unweighted</strong> algorithm that does not guarantee the shortest path.
+          </p>
         </TabPanel>
         <TabPanel value={value} index={2}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec molestie odio. Maecenas volutpat lacus at
-          sagittis ultrices. Vivamus viverra mi vel neque hendrerit congue. Sed lobortis nec tortor sit amet eleifend.
-          Vestibulum ultrices sollicitudin aliquet. Praesent dolor justo, sollicitudin a faucibus in, aliquam vel augue.
-          Nam facilisis scelerisque justo sed lacinia. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-          posuere cubilia curae; Sed pulvinar id nisl in malesuada. Praesent posuere mauris sed justo elementum, gravida
-          efficitur nisi tincidunt. Morbi posuere sit amet purus quis egestas. Donec ac lacus quam. Praesent eleifend
-          risus dui, ut tempor quam vulputate in. Maecenas eu sapien cursus ante pharetra blandit. Sed ultricies
-          facilisis dictum. Vestibulum luctus dui id elit gravida, a pellentesque purus pharetra. Aliquam lobortis
-          volutpat neque, ac luctus lectus ullamcorper et. Suspendisse tempor turpis id tortor molestie, non rhoncus
-          lacus egestas. Ut hendrerit, tellus non venenatis fringilla, arcu ipsum tincidunt arcu, ut condimentum sapien
-          est sit amet dui. Maecenas turpis libero, sollicitudin eu eros et, eleifend molestie lorem. Maecenas iaculis
-          pulvinar purus. Duis gravida tempor felis vel sollicitudin. Nam ut posuere dui. Pellentesque vulputate, arcu
-          eu mattis rhoncus, lorem risus vulputate nunc, sed condimentum tortor arcu eu neque. Aliquam dignissim sapien
-          non ligula malesuada, in vestibulum metus sagittis. Mauris imperdiet odio magna, id consectetur libero auctor
-          eleifend. Suspendisse potenti. Nunc venenatis et ante sed gravida. Nunc sit amet augue sit amet dui mollis
-          ornare vel non justo. Fusce ac aliquam eros. Donec maximus cursus mauris, sit amet viverra quam eleifend ac.
-          Phasellus nunc odio, blandit euismod sollicitudin ut, fringilla nec quam. Etiam ultricies in lectus nec
-          fringilla. Vestibulum porttitor pellentesque arcu, a euismod nulla mattis ac. Nunc sed sem egestas, blandit
-          ipsum nec, tempus nunc. Donec at augue ligula. Fusce nec vestibulum nunc. Orci varius natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Integer congue risus velit. Aenean elementum, nisl a
-          luctus vehicula, elit augue dignissim justo, at volutpat eros nisl nec mi. Vestibulum aliquam risus nec
-          rhoncus suscipit. Curabitur non eleifend dui. Integer vitae justo a eros blandit tempus. Vestibulum bibendum,
-          dui et pulvinar rutrum, purus arcu ultrices ipsum, id auctor ex ligula at justo. Nulla mollis, sapien quis
-          ultricies sagittis, leo risus posuere elit, ac tincidunt massa metus nec urna. Phasellus a ligula eleifend dui
-          vestibulum venenatis. Duis pulvinar sem facilisis pulvinar suscipit. Proin enim tellus, porta id lectus
-          facilisis, porta pulvinar tellus. Nam non gravida felis. Nullam vulputate vitae turpis ac tempus. Sed urna
-          nulla, sagittis in commodo vitae, venenatis quis nisl. Pellentesque consectetur eros ac blandit porta. Sed sit
-          amet nunc eget enim pharetra accumsan. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Curabitur in convallis quam. Vivamus et odio at sapien ornare auctor id ac diam. Sed
-          sollicitudin, diam ut volutpat tristique, nunc enim laoreet velit, eu ullamcorper augue nulla eu purus. Cras
-          sed mauris turpis. Duis vehicula ultrices aliquet. Proin at rhoncus magna. Duis non nisi ut sem ullamcorper
-          interdum sit amet a nulla. Aliquam erat volutpat. Nullam ac feugiat ligula. Nulla facilisi. Pellentesque
-          habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+          <p>Coming soon!</p>
         </TabPanel>
       </DialogContent>
       <DialogActions>
