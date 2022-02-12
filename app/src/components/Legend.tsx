80ebeca5a0f30602b10780182cd37dd085244a91
Robert Herber <rwherber@gmail.com>
2022-02-11 18:49:54 -0800

    chore: update mobile legend

--- app/src/components/Legend.tsx
+++ app/src/components/Legend.tsx
@@ -1,7 +1,21 @@
 import { Close } from '@mui/icons-material';
-import { AppBar, Button, Dialog, Divider, IconButton, Grid, Slide, Stack, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Button,
+  Dialog,
+  Divider,
+  IconButton,
+  Grid,
+  Slide,
+  Stack,
+  Toolbar,
+  Typography,
+  Card,
+  Box,
+  DialogContent
+} from '@mui/material';
 import { TransitionProps } from '@mui/material/transitions';
-import { CSSProperties, forwardRef, ReactElement, Ref, Fragment } from 'react';
+import { CSSProperties, forwardRef, ReactElement, Ref } from 'react';
 
 const Transition = forwardRef(function Transition(
   props: TransitionProps & {
@@ -19,11 +33,14 @@ export interface LegendProps {
 }
 
 export default function Legend({ isDesktop, open, handleClose }: LegendProps): JSX.Element {
+  const width = isDesktop ? '25px' : '75px';
+  const height = isDesktop ? '25px' : '75px';
+
   const style: CSSProperties = {
-    minWidth: '25px',
-    minHeight: '25px',
-    width: '25px',
-    height: '25px'
+    minWidth: width,
+    minHeight: height,
+    width,
+    height
   };
 
   const legendItems = [
@@ -38,7 +55,7 @@ export default function Legend({ isDesktop, open, handleClose }: LegendProps): J
   ];
 
   const mobileLegend = (
-    <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+    <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition} scroll="paper">
       <AppBar sx={{ position: 'relative' }}>
         <Toolbar>
           <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
@@ -52,21 +69,29 @@ export default function Legend({ isDesktop, open, handleClose }: LegendProps): J
           </Button>
         </Toolbar>
       </AppBar>
-      <Grid container spacing={2} sx={{ paddingLeft: 2, paddingRight: 2 }}>
-        {legendItems.map(([text, className]) => {
-          return (
-            <Fragment key={className}>
-              <Grid item xs={10} sm={4} display="flex" sx={{ alignItems: 'center' }}>
-                <p>{text}</p>
-              </Grid>
+      <DialogContent sx={{ padding: 2 }}>
+        <Grid container spacing={2}>
+          {legendItems.map(([text, className]) => {
+            return (
+              <Grid item key={className} xs={12}>
+                <Card>
+                  <Box sx={{ margin: 2 }}>
+                    <Stack direction="row" display="flex">
+                      <Stack>
+                        <Typography variant="h6">{text}</Typography>
+                      </Stack>
 
-              <Grid item xs={2} display="flex" sx={{ alignItems: 'center', justifyContent: 'right' }}>
-                <span style={style} className={className} />
+                      <Stack sx={{ marginLeft: 'auto', alignItems: 'center', justifyContent: 'center' }}>
+                        <span style={style} className={className} />
+                      </Stack>
+                    </Stack>
+                  </Box>
+                </Card>
               </Grid>
-            </Fragment>
-          );
-        })}
-      </Grid>
+            );
+          })}
+        </Grid>
+      </DialogContent>
     </Dialog>
   );
 
